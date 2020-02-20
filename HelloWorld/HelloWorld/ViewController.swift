b862e32f7e86e835a98a19194ff7f8abe9e5a5c8
Haydn Wiese <haydnwiese@gmail.com>
2020-02-20 18:51:10 -0500

    Added additional buttons with emojis

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -16,13 +16,18 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome to My First App",
-                                                message: "Hello World",
-                                                preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK",
-                                                style: UIAlertAction.Style.default,
-                                                handler: nil))
-        present(alertController, animated: true, completion: nil)
+        
+        if let label = sender.titleLabel?.text {
+            var description: String;
+            
+            let alertController = UIAlertController(title: "Meaning",
+                                                    message: label,
+                                                    preferredStyle: UIAlertController.Style.alert)
+            alertController.addAction(UIAlertAction(title: "OK",
+                                                    style: UIAlertAction.Style.default,
+                                                    handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
     }
 
 }
