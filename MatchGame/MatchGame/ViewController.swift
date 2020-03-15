4e6dea0bd7b06fef0613e36622a8ad3495c9930f
Haydn Wiese <haydnwiese@gmail.com>
2020-03-15 14:20:45 -0400

    Implemented UICollectionViewDelegate and UICOllectionViewDataSource protocols

--- MatchGame/MatchGame/ViewController.swift
+++ MatchGame/MatchGame/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var model = CardModel()
     var cardArray = [Card]()
@@ -17,7 +19,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         cardArray = model.getCards()
-    }
 
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    // MARK: - UICollectionView Protocol Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
 }
 
