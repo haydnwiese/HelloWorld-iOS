b524576ca5cdc0ffacd23b46921fc36ba6ceae54
Haydn Wiese <haydnwiese@gmail.com>
2020-03-15 00:58:36 -0400

    Created new project for dice app

---
+++ Dice/Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Haydn Wiese on 2020-03-15.
+//  Copyright © 2020 Haydn Wiese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
