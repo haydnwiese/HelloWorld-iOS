a7f058ac498002a4addf2ab984575461be4ce891
Haydn Wiese <haydnwiese@gmail.com>
2020-03-15 01:25:26 -0400

    Added rolling dice functionality

--- Dice/Dice/ViewController.swift
+++ Dice/Dice/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        overrideUserInterfaceStyle = .light
     }
 
-
+    @IBAction func rollDice(_ sender: Any) {
+        let number = (1...6).randomElement()!
+        self.diceImageView.image = UIImage(named: "dice-\(number)")
+    }
+    
 }
 
