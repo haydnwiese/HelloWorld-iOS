46d6fa382c86b0e19f5f54e430a8cac0fe94cb08
Haydn Wiese <haydnwiese@gmail.com>
2020-03-19 23:49:42 -0400

    Created new project to test table view with multi-screen app

---
+++ MultiScreenTableView/MultiScreenTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiScreenTableView
+//
+//  Created by Haydn Wiese on 2020-03-19.
+//  Copyright © 2020 Haydn Wiese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
