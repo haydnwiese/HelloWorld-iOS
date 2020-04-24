9173161e6d9614436e0e22032ea1540b8b1ad41d
Haydn Wiese <haydnwiese@gmail.com>
2020-04-23 23:39:48 -0400

    Created data model

---
+++ FoodTracker/FoodTracker/Meal.swift
@@ -0,0 +1,33 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Haydn Wiese on 2020-04-23.
+//  Copyright © 2020 Haydn Wiese. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    init?(name: String, photo: UIImage, rating: Int) {
+        // Name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // Rating must be between 0 and 5
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
