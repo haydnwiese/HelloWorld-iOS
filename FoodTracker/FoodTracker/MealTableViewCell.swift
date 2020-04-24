fcf3c51fad328d6640140a1bff4734bf64125a02
Haydn Wiese <haydnwiese@gmail.com>
2020-04-24 16:23:48 -0400

    Created table view cell

---
+++ FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Haydn Wiese on 2020-04-24.
+//  Copyright © 2020 Haydn Wiese. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
