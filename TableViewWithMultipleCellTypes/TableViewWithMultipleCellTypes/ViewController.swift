d2b469f1f081001765635c1d3a96047efe763c1d
Haydn Wiese <haydnwiese@gmail.com>
2020-05-15 15:54:04 -0400

    Created TableViewWithMultipleCellTypes project

---
+++ TableViewWithMultipleCellTypes/TableViewWithMultipleCellTypes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewWithMultipleCellTypes
+//
+//  Created by Haydn Wiese on 2020-05-15.
+//  Copyright © 2020 Haydn Wiese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
