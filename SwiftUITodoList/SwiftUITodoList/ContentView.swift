5e04207cd0f3c4a83a8a13b4b701f5b6e0386a54
Haydn Wiese <haydnwiese@gmail.com>
2020-11-09 00:18:48 -0500

    Created SwiftUITodoList

---
+++ SwiftUITodoList/SwiftUITodoList/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUITodoList
+//
+//  Created by Haydn Wiese on 2020-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            
+    }
+}
---
+++ SwiftUITodoList/SwiftUITodoList/SwiftUITodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITodoListApp.swift
+//  SwiftUITodoList
+//
+//  Created by Haydn Wiese on 2020-11-09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
