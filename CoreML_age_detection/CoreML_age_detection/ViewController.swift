fe9be05509895618f2b38a4305c526276c77e47b
Haydn Wiese <haydnwiese@gmail.com>
2020-12-20 15:34:37 -0500

    Created CoreML_age_detection

---
+++ CoreML_age_detection/CoreML_age_detection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreML_age_detection
+//
+//  Created by Haydn Wiese on 2020-12-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
