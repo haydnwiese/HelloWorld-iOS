e1faa605ab9fcbfdb3f6d41ac92d6f1d7b412442
Haydn Wiese <haydnwiese@gmail.com>
2020-12-27 21:47:04 -0500

    Created HuliPizza Project

---
+++ HuliPizza/HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Haydn Wiese on 2020-12-27.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
